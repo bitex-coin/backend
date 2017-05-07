5fc4d3ff054b93777754c3e536a2994ba4235c67
bzero <cderivan@gmail.com>
2017-05-07 17:09:24 +0000

    remove non-sense log

--- ws_gateway/main.py
+++ ws_gateway/main.py
@@ -628,7 +628,7 @@ class WebSocketGatewayApplication(tornado.web.Application):
             response = urlopen("https://torstatus.blutmagie.de/ip_list_all.php/Tor_ip_list_ALL.csv")
             self.tor_ip_list_ = set(response.read().splitlines())
             #self.tor_ip_list_.add('127.0.0.1')
-            self.log('DEBUG', 'TOR_LIST', str(self.tor_ip_list_))
+            #self.log('DEBUG', 'TOR_LIST', str(self.tor_ip_list_))
         except:
             pass
 
